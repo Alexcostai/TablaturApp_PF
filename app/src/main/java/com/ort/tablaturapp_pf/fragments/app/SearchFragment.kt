abdfac4b107b841b27bddbe0a1804a6bbaa90076
Gonzalo <gga1999@live.com.ar>
2022-05-29 22:56:30 -0300

    hardcoded search test implementation

--- app/src/main/java/com/ort/tablaturapp_pf/fragments/app/SearchFragment.kt
+++ app/src/main/java/com/ort/tablaturapp_pf/fragments/app/SearchFragment.kt
@@ -1,11 +1,13 @@
 package com.ort.tablaturapp_pf.fragments.app
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.*
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
 import com.ort.tablaturapp_pf.R
 import com.ort.tablaturapp_pf.viewmodels.SearchViewModel
 
@@ -16,18 +18,63 @@ class SearchFragment : Fragment() {
     }
 
     private lateinit var viewModel: SearchViewModel
+    private lateinit var contentView: View
+    private lateinit var adapter: ArrayAdapter<*>
+    private lateinit var activityContext: Context
+    private lateinit var listView: ListView
+    private lateinit var emptyTextView: TextView
+
+    override fun onAttach( context: Context) {
+        super.onAttach(context)
+        activityContext = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.search_fragment, container, false)
+
+        contentView = inflater.inflate(R.layout.search_fragment, container, false)
+        listView = contentView.findViewById(R.id.lv_searchResultSongs)
+        emptyTextView = contentView.findViewById(R.id.tv_emptyTextView)
+        return contentView
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
+        adapter = ArrayAdapter(activityContext, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.countries_array))
+        listView.adapter = adapter
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            Toast.makeText(activityContext, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+        }
+        listView.emptyView = emptyTextView
         // TODO: Use the ViewModel
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.nav_search_menu, menu)
+        val search = menu?.findItem(R.id.nav_search_menu)
+        val searchView = search?.actionView as SearchView
+        searchView.queryHint = "Buscar canción..."
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return true
+            }
+        })
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
 }
