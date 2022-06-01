0c1823db0639aa0e97df4c6adcd39dce6b31332c
Alex Costa <alex@peyobook.local>
2022-06-01 00:16:27 -0300

    Implementacion inicial de creacion de ruta de aprendizaje

--- app/src/main/java/com/ort/tablaturapp_pf/fragments/app/CreateLearningPathFragment.kt
+++ app/src/main/java/com/ort/tablaturapp_pf/fragments/app/CreateLearningPathFragment.kt
@@ -1,22 +1,31 @@
 package com.ort.tablaturapp_pf.fragments.app
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.CheckBox
-import android.widget.Spinner
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.ort.tablaturapp_pf.R
 import com.ort.tablaturapp_pf.viewmodels.CreateLearningPathViewModel
+import kotlinx.coroutines.*
+import org.json.JSONObject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
 
 class CreateLearningPathFragment : Fragment() {
 
     lateinit var createLearningPathView: View
-    lateinit var instrumentSpn: Spinner
+    lateinit var createLearningPathBtn: Button
 
     companion object {
         fun newInstance() = CreateLearningPathFragment()
@@ -30,18 +39,7 @@ class CreateLearningPathFragment : Fragment() {
     ): View? {
         createLearningPathView =
             inflater.inflate(R.layout.create_learning_path_fragment, container, false)
-        instrumentSpn = createLearningPathView.findViewById(R.id.instrumentSpn)
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.instruments_list,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            instrumentSpn.adapter = adapter
-        }
+        createLearningPathBtn = createLearningPathView.findViewById(R.id.createLearningPathBtn)
         return createLearningPathView;
     }
 
@@ -62,12 +60,64 @@ class CreateLearningPathFragment : Fragment() {
                         ).show()
                         it.isChecked = false;
                     }
-                } else{
+                } else {
                     selectedGenres.remove(it.text.toString().lowercase())
                 }
             }
         }
+
+        createLearningPathBtn.setOnClickListener {
+            if (selectedGenres.size < 1) {
+                Toast.makeText(
+                    requireContext(),
+                    "Seleccioná al menos 1 género",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                var playlistIds = arrayListOf<String>()
+                val quantitySongsByGenre = getQuantitySongsByGenre(selectedGenres);
+                GlobalScope.launch(Dispatchers.Main) {
+                    playlistIds.add(getPlaylistIdByGenre(selectedGenres[0].lowercase()))
+                    Log.d("TestPlaylistIds", playlistIds.toString())
+                }
+            }
+        }
     }
+
+
+    suspend fun getPlaylistIdByGenre(genre: String) = suspendCoroutine<String> { cont ->
+        val queue = Volley.newRequestQueue(context)
+        val url =
+            "https://api.spotify.com/v1/browse/categories/$genre/playlists?country=AR&limit=1"
+        val stringRequest = object : StringRequest(Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                val playlistId =
+                    JSONObject(response).getJSONObject("playlists").getJSONArray("items")
+                        .getJSONObject(0).getString("id")
+                cont.resume(playlistId)
+            },
+            Response.ErrorListener { error -> Log.e("SpotifyError", error.toString()) }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-Type"] = "application/json"
+                headers["Authorization"] =
+                    "Bearer BQA6hEDG2f45CRDFH0vgjDEnDKRvhg78VwC2sx-8nBkEevqjtzdiBakGfBB_5P2UqiDhgCgJ7Dg6PmZA2ZGTRROEjoKFiQ7jKZANgSsRTiUUdV5ng_OA8faxHXoj8qIcbCBrLbFq8_MkGYFhR93pgvnqAE1BEgH-TE4"
+                return headers
+            }
+        }
+        queue.add(stringRequest)
+    }
+
+
+    private fun getQuantitySongsByGenre(selectedGenres: MutableList<String>): IntArray {
+        var quantitySongsByGenre = intArrayOf(10);
+        when (selectedGenres.size) {
+            2 -> quantitySongsByGenre = intArrayOf(5, 5)
+            3 -> quantitySongsByGenre = intArrayOf(3, 3, 4)
+        }
+        return quantitySongsByGenre
+    }
+
 
     private fun getCheckBoxGenres(): MutableList<CheckBox> {
         val checkBoxGenres = mutableListOf<CheckBox>()
