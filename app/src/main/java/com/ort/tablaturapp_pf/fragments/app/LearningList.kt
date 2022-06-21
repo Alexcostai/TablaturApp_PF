ad2282f5165eb03cc0d15e642243dc01924e7ee8
Gonzalo <gga1999@live.com.ar>
2022-06-20 21:29:07 -0300

    se agrega boton de generar nuevamente ruta, dentro de la ruta ya generada

--- app/src/main/java/com/ort/tablaturapp_pf/fragments/app/LearningList.kt
+++ app/src/main/java/com/ort/tablaturapp_pf/fragments/app/LearningList.kt
@@ -1,22 +1,29 @@
 package com.ort.tablaturapp_pf.fragments.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.ort.tablaturapp_pf.R
 
 
 class LearningList : Fragment() {
   lateinit var LearningListView: View
-  lateinit var listView1: ListView
-  lateinit var songs : Array<String>
-  lateinit var ids : Array<String>
+  private lateinit var listView1: ListView
+  private lateinit var songs : Array<String>
+  private lateinit var ids : Array<String>
+  private lateinit var learningListBtn: Button
+  private var isPremium: Boolean = false
+
+  private val auth = Firebase.auth;
+  private val db = Firebase.firestore
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +32,7 @@ class LearningList : Fragment() {
     // Inflate the layout for this fragment
     LearningListView = inflater.inflate(R.layout.fragment_learning_list, container, false)
     listView1 = LearningListView.findViewById(R.id.lv1)
+    learningListBtn = LearningListView.findViewById(R.id.learningList_button)
 
     return LearningListView
   }
@@ -32,8 +40,18 @@ class LearningList : Fragment() {
   override fun onActivityCreated(savedInstanceState: Bundle?) {
     super.onActivityCreated(savedInstanceState)
 
+    db.collection("users").document(auth.currentUser!!.uid).get().addOnSuccessListener {
+      user ->
+      if (user != null) {
+        isPremium = user["isPremium"] as Boolean
+      }
+    }
+
     listView1.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
       goToFragment(SongFragment(), ids[position])
+    }
+    learningListBtn.setOnClickListener {
+      goToFragment(CreateLearningPathFragment(), isPremium)
     }
   }
 
@@ -54,6 +72,17 @@ class LearningList : Fragment() {
       val clpFragment: Fragment = fragment
       val arguments = Bundle()
       arguments.putString("song_id", song_id)
+      clpFragment.arguments = arguments
+      replace(R.id.navAppController, clpFragment)
+      commit()
+    }
+  }
+
+  private fun goToFragment(fragment: Fragment, isPremium: Boolean){
+    parentFragmentManager.beginTransaction().apply {
+      val clpFragment: Fragment = fragment
+      val arguments = Bundle()
+      arguments.putBoolean("isPremium", isPremium)
       clpFragment.arguments = arguments
       replace(R.id.navAppController, clpFragment)
       commit()
